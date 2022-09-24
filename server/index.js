e5804c717d80aaf7b51aa335a6f528f88c2fb243
basavarajh99 <basavarajh7299@gmail.com>
2022-09-24 20:44:13 +0530

    Server setup

--- server/index.js
+++ server/index.js
@@ -2,4 +2,28 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import postRoutes from './routes/posts.js'
+
+const app = express();
+
+app.use('/posts', postRoutes);
+
+app.use(bodyParser.json({ limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+
+app.use(cors());
+
+//mongo connect
+
+const CONNECTION_URL = "mongodb+srv://Basavaraj:Abcd%401234@freecodecamp.xgxjbef.mongodb.net/memoriesDB?retryWrites=true&w=majority";
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.error(error));
+
+
+
+
 
