e7bbd72b350898dd0c896af71c502be0888909a0
basavarajh99 <basavarajh7299@gmail.com>
2022-09-26 15:04:04 +0530

    make it better

---
+++ index.js
@@ -0,0 +1,36 @@
+import * as dotenv from 'dotenv';
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import postRoutes from './routes/posts.js'
+
+dotenv.config()
+const app = express();
+
+
+app.use(bodyParser.json({ limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+
+app.use(cors());
+
+app.use('/posts', postRoutes);
+
+app.get("/", (req, res) => {
+    res.send("You are in Memories");
+})
+
+//mongo connect
+
+// const CONNECTION_URL = "mongodb+srv://Basavaraj:Abcd%401234@freecodecamp.xgxjbef.mongodb.net/memoriesDB?retryWrites=true&w=majority";
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.error(error));
+
+
+
+
+
