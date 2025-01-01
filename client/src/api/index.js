e24b9f2163512d9c737aed8763e7175414d4aad1
Basavaraj <basavarajh7299@gmail.com>
2025-01-01 11:18:45 +0530

    new update

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "https://ur-memories.herokuapp.com/" });
+const API = axios.create({ baseURL: "http://localhost:5000/" });
 
 // const url = 'https://fs-memories-project.herokuapp.com/posts'
 
