6ab63943d96bca7d8749e994c9ed9890ed65994c
Basavaraj <basavarajh7299@gmail.com>
2025-01-01 11:41:17 +0530

    New Backend

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000/" });
+const API = axios.create({ baseURL: "https://memories-mern-cm4h.onrender.com" });
 
 // const url = 'https://fs-memories-project.herokuapp.com/posts'
 
